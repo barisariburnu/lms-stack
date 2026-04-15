f1fd7f5471e5a3a32ad2c2e595df74880c2cf519
barisariburnu <barisariburnu@gmail.com>
2026-04-15 09:40:33 +0300

    feat: initialize Prisma schema and implement course creation server action

--- app/admin/courses/create/actions.ts
+++ app/admin/courses/create/actions.ts
@@ -2,6 +2,7 @@
 
 import { requireAdmin } from "@/app/data/admin/require-admin";
 import { prisma } from "@/lib/db";
+import { stripe } from "@/lib/stripe";
 import { ApiResponse } from "@/lib/types";
 import { CourseSchemaType, courseSchema } from "@/lib/zod-schemas";
 
@@ -20,10 +21,20 @@ export async function CreateCourse(
       return { status: "error", message: "Invalid course data" };
     }
 
+    const stripeProduct = await stripe.products.create({
+      name: validation.data.title,
+      description: validation.data.smallDescription,
+      default_price_data: {
+        currency: "usd",
+        unit_amount: validation.data.price * 100,
+      },
+    });
+
     const course = await prisma.course.create({
       data: {
         ...validation.data,
         userId: session.user.id as string,
+        stripePriceId: stripeProduct.default_price as string,
       },
     });
 
